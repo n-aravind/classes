ed5b9febd0ac8005014374b56ce1a2f5199e2402
Aravind <aravindnareddy@gmail.com>
2019-01-27 15:18:01 -0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyFirstClass myFirstClass = new MyFirstClass();
+        System.out.println(myFirstClass.count);
+        System.out.println(myFirstClass.name);
+        System.out.println(myFirstClass.flagemember);
+    }
+}
---
+++ src/MyFirstClass.java
@@ -0,0 +1,6 @@
+public class MyFirstClass{
+    int count = 10;
+    String name = "Java";
+    boolean flagemember = false;
+
+}
